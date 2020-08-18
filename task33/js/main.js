77cb7b8e47d357ddaa64433927badd59d5c0df8e
Artem <a.kosherenkov@gmail.com>
2020-08-18 23:48:19 +0300

    slider on js

---
+++ task33/js/main.js
@@ -0,0 +1,9 @@
+
+
+$('.product__slider').slick({
+    prevArrow: '<button type="button" class="slick-prev"><img src="../img/arrow_left.png"></button>',
+    nextArrow: '<button type="button" class="slick-prev"><img src="../img/arrow_right.png"></button>',
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1
+})
