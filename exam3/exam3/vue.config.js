178b6c6420287c6195c7b18230e1009299f9e7da
Artem <a.kosherenkov@gmail.com>
2020-10-06 01:03:12 +0300

    change vue config file

--- exam3/exam3/vue.config.js
+++ exam3/exam3/vue.config.js
@@ -6,5 +6,5 @@ module.exports = {
       }
     }
   },
-  publicPath: process.env.NODE_ENV === "production" ? "/exam3/exam3/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/beetroot/" : "/"
 };
